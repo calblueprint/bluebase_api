7e92d7af5b94d58f5943755fc4c44869b0798495
Tricia Fu <triciasfu@gmail.com>
2015-03-02 00:45:58 -0800

    finished rails api application -- need to make the gem now

--- rails-API-app/app/controllers/application_controller.rb
+++ rails-API-app/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # For applications, you may want to use :exception instead.
+  protect_from_forgery with: :null_session
 end
--- rails-API-app/config/routes.rb
+++ rails-API-app/config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1 do
+      # Future API routes here
+    end
+  end
 end
